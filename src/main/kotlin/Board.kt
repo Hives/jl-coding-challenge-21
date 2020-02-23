5b0754346fa3cc62a196d341a20e53eb318f4126
paul.martin <paul.y.martin@johnlewis.co.uk>
2020-02-23 00:55:58 +0000

    Create deducer function

--- src/main/kotlin/Board.kt
+++ src/main/kotlin/Board.kt
@@ -12,3 +12,22 @@ data class Board(val squares: List<Int>) {
     private fun colIndexOf(position: Int) = position % 9
     private fun subGridIndexOf(position: Int) = (colIndexOf(position) / 3) + (3 * (rowIndexOf(position) / 3))
 }
+
+fun deducer(board: Board): Board = Board(board.squares.mapIndexed { index, value ->
+    if (value == 0) {
+        val numbersInRow = board.getRowContaining(index).filter { it != 0 }
+        val numbersInColumn = board.getColumnContaining(index).filter { it != 0 }
+        val numbersInSubGrid = board.getSubGridContaining(index).filter { it != 0 }
+
+        val unusedNumbers =
+            (1..9).toList().filter { !(numbersInRow + numbersInColumn + numbersInSubGrid).contains(it) }
+
+        if (unusedNumbers.size == 1) {
+            unusedNumbers.single()
+        } else {
+            0
+        }
+    } else {
+        value
+    }
+})
---
+++ src/test/kotlin/DeducerTest.kt
@@ -0,0 +1,261 @@
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+object DeducerTest : Spek({
+    describe("deducer") {
+        context("If it can't make a deduction") {
+            val board = Board(
+                listOf(
+                    0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0
+                )
+            )
+            it("returns the same board") {
+                assertThat(deducer(board)).isEqualTo(board)
+            }
+        }
+
+        describe("rows") {
+            context("If every square but one in the first row is complete") {
+                val board = Board(
+                    listOf(
+                        1, 2, 3, 4, 0, 6, 7, 8, 9,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0
+                    )
+                )
+                it("it returns a board with the first row completed") {
+                    val expectedBoard = Board(
+                        listOf(
+                            1, 2, 3, 4, 5, 6, 7, 8, 9,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0
+                        )
+                    )
+                    assertThat(deducer(board)).isEqualTo(expectedBoard)
+                }
+            }
+
+            context("If every square but one in the last row is complete") {
+                val board = Board(
+                    listOf(
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        9, 8, 7, 6, 5, 4, 3, 0, 1
+                    )
+                )
+                it("it returns a board with the first row completed") {
+                    val expectedBoard = Board(
+                        listOf(
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            9, 8, 7, 6, 5, 4, 3, 2, 1
+                        )
+                    )
+                    assertThat(deducer(board)).isEqualTo(expectedBoard)
+                }
+            }
+        }
+
+        describe("columns") {
+            context("If every square but one in the first column is complete") {
+                val board = Board(
+                    listOf(
+                        1, 0, 0, 0, 0, 0, 0, 0, 0,
+                        2, 0, 0, 0, 0, 0, 0, 0, 0,
+                        3, 0, 0, 0, 0, 0, 0, 0, 0,
+                        4, 0, 0, 0, 0, 0, 0, 0, 0,
+                        5, 0, 0, 0, 0, 0, 0, 0, 0,
+                        6, 0, 0, 0, 0, 0, 0, 0, 0,
+                        7, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        9, 0, 0, 0, 0, 0, 0, 0, 0
+                    )
+                )
+                it("it returns a board with the first column completed") {
+                    val expectedBoard = Board(
+                        listOf(
+                            1, 0, 0, 0, 0, 0, 0, 0, 0,
+                            2, 0, 0, 0, 0, 0, 0, 0, 0,
+                            3, 0, 0, 0, 0, 0, 0, 0, 0,
+                            4, 0, 0, 0, 0, 0, 0, 0, 0,
+                            5, 0, 0, 0, 0, 0, 0, 0, 0,
+                            6, 0, 0, 0, 0, 0, 0, 0, 0,
+                            7, 0, 0, 0, 0, 0, 0, 0, 0,
+                            8, 0, 0, 0, 0, 0, 0, 0, 0,
+                            9, 0, 0, 0, 0, 0, 0, 0, 0
+                        )
+                    )
+                    assertThat(deducer(board)).isEqualTo(expectedBoard)
+                }
+            }
+
+            context("If every square but one in the last column is complete") {
+                val board = Board(
+                    listOf(
+                        0, 0, 0, 0, 0, 0, 0, 0, 9,
+                        0, 0, 0, 0, 0, 0, 0, 0, 8,
+                        0, 0, 0, 0, 0, 0, 0, 0, 7,
+                        0, 0, 0, 0, 0, 0, 0, 0, 6,
+                        0, 0, 0, 0, 0, 0, 0, 0, 5,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 3,
+                        0, 0, 0, 0, 0, 0, 0, 0, 2,
+                        0, 0, 0, 0, 0, 0, 0, 0, 1
+                    )
+                )
+                it("it returns a board with the last column completed") {
+                    val expectedBoard = Board(
+                        listOf(
+                            0, 0, 0, 0, 0, 0, 0, 0, 9,
+                            0, 0, 0, 0, 0, 0, 0, 0, 8,
+                            0, 0, 0, 0, 0, 0, 0, 0, 7,
+                            0, 0, 0, 0, 0, 0, 0, 0, 6,
+                            0, 0, 0, 0, 0, 0, 0, 0, 5,
+                            0, 0, 0, 0, 0, 0, 0, 0, 4,
+                            0, 0, 0, 0, 0, 0, 0, 0, 3,
+                            0, 0, 0, 0, 0, 0, 0, 0, 2,
+                            0, 0, 0, 0, 0, 0, 0, 0, 1
+                        )
+                    )
+                    assertThat(deducer(board)).isEqualTo(expectedBoard)
+                }
+            }
+        }
+
+        describe("subgrids") {
+            context("If every square but one in the first subgrid is complete") {
+                val board = Board(
+                    listOf(
+                        1, 2, 3, 0, 0, 0, 0, 0, 0,
+                        0, 5, 6, 0, 0, 0, 0, 0, 0,
+                        7, 8, 9, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0
+                    )
+                )
+                it("it returns a board with the first subgrid completed") {
+                    val expectedBoard = Board(
+                        listOf(
+                            1, 2, 3, 0, 0, 0, 0, 0, 0,
+                            4, 5, 6, 0, 0, 0, 0, 0, 0,
+                            7, 8, 9, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0
+                        )
+                    )
+                    assertThat(deducer(board)).isEqualTo(expectedBoard)
+                }
+            }
+
+            context("If every square but one in the last subgrid is complete") {
+                val board = Board(
+                    listOf(
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 1, 4, 7,
+                        0, 0, 0, 0, 0, 0, 2, 0, 8,
+                        0, 0, 0, 0, 0, 0, 3, 6, 9
+                    )
+                )
+                it("it returns a board with the first subgrid completed") {
+                    val expectedBoard = Board(
+                        listOf(
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 1, 4, 7,
+                            0, 0, 0, 0, 0, 0, 2, 5 , 8,
+                            0, 0, 0, 0, 0, 0, 3, 6, 9
+                        )
+                    )
+                    assertThat(deducer(board)).isEqualTo(expectedBoard)
+                }
+            }
+
+        }
+
+        describe("rows, columns + subgrids together") {
+            context("If a square's row, column and subgrid contain all but one of the numbers 1-9") {
+                val board = Board(
+                    listOf(
+                        0, 0, 0, 0, 3, 0, 0, 0, 0,
+                        0, 0, 0, 0, 4, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 6, 0, 7, 0, 0, 0,
+                        1, 0, 0, 0, 0, 0, 0, 2, 0,
+                        0, 0, 0, 0, 0, 8, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 5, 0, 0, 0, 0
+                    )
+                )
+                it("it returns a board with that square completed") {
+                    val expectedBoard = Board(
+                        listOf(
+                            0, 0, 0, 0, 3, 0, 0, 0, 0,
+                            0, 0, 0, 0, 4, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 6, 0, 7, 0, 0, 0,
+                            1, 0, 0, 0, 9, 0, 0, 2, 0,
+                            0, 0, 0, 0, 0, 8, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 5, 0, 0, 0, 0
+                        )
+                    )
+                    assertThat(deducer(board)).isEqualTo(expectedBoard)
+                }
+            }
+
+        }
+    }
+})
